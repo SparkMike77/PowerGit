f6c77f38c55785348a4772c3c35926f855777af0
Mike Plambeck <michael.plambeck@gmail.com>
2018-06-11 16:33:40 -0600

    Create TheScriptThatMakesMeSad.ps1
    Powershell script that never fails to make me sad.
    
    Requires https://www.powershellgallery.com/packages/DSInternals/2.22 to be installed
    
    Do not run this on a day you are feeling happy or on a day when you feel sad.
    
    I am Sorry.

---
+++ TheScriptThatMakesMeSad.ps1
@@ -0,0 +1,12 @@
+#Smart guy I 
+#http://www.stephenthompson.tech/audit-ad-passwords-with-powershell/
+#Install-Module -Name DSInternals #As Administrator\
+#https://www.powershellgallery.com/packages/DSInternals/2.22
+Import-Module DSInternals
+ 
+$DictFile = "C:\temp\pass.txt"
+$DC = "Domain Controller"
+$Domain = "DC=Something,DC=Clever"
+$Dict = Get-Content $DictFile | ConvertTo-NTHashDictionary
+ 
+Get-ADReplAccount -All -Server $DC -NamingContext $Domain | Test-PasswordQuality -WeakPasswordHashes $Dict -ShowPlainTextPasswords -IncludeDisabledAccounts | out-file c:\temp\Password.txt

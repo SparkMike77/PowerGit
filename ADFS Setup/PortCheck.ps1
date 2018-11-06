efebaa7fb98b11c097b3f3763a864b7886daf64c
Mike Plambeck <michael.plambeck@gmail.com>
2018-11-06 09:36:31 -0700

    Create PortCheck.ps1
    Requires PS v5+, which means server 2021R2+
    If you run this from the WAP/Proxy  run it as is
    If you run this from the ADFS node, swap the variable in the foreach loop from $adfsbox to $adfsProxy

---
+++ ADFS Setup/PortCheck.ps1
@@ -0,0 +1,55 @@
+$adfsbox = "0.0.0.0" \\IP for internal ADFS node goes here
+$adfsProxy="0.0.0.0" \\IP for Proxy (in your DMZ) goes here
+$ports = "49443","443","80","5985","5986"
+
+foreach ($port in $ports)
+{
+    Test-NetConnection -Port $port -ComputerName $adfsbox
+}
+
+\\Output should look like:
+\\
+\\
+\\ComputerName           : [ADFS NODE IP]
+\\RemoteAddress          : [ADFS NODE IP]
+\\RemotePort             : 49443
+\\InterfaceAlias         : Ethernet0
+\\SourceAddress          : [PROXY IP]
+\\PingSucceeded          : True
+\\PingReplyDetails (RTT) : 0 ms
+\\TcpTestSucceeded       : True
+
+\\ComputerName     : [ADFS NODE IP]
+\\RemoteAddress    : [ADFS NODE IP]
+\\RemotePort       : 443
+\\InterfaceAlias   : Ethernet0
+\\SourceAddress    : [PROXY IP]
+\\TcpTestSucceeded : True
+
+\\ComputerName           : [ADFS NODE IP]
+\\RemoteAddress          : [ADFS NODE IP]
+\\RemotePort             : 80
+\\InterfaceAlias         : Ethernet0
+\\SourceAddress          : [PROXY IP]
+\\PingSucceeded          : True
+\\PingReplyDetails (RTT) : 0 ms
+\\TcpTestSucceeded       : True
+
+\\ComputerName           : [ADFS NODE IP]
+\\RemoteAddress          : [ADFS NODE IP]
+\\RemotePort             : 5985
+\\InterfaceAlias         : Ethernet0
+\\SourceAddress          : [PROXY IP]
+\\PingSucceeded          : True
+\\PingReplyDetails (RTT) : 0 ms
+\\TcpTestSucceeded       : True
+
+\\ComputerName           : [ADFS NODE IP]
+\\RemoteAddress          : [ADFS NODE IP]
+\\RemotePort             : 5986
+\\InterfaceAlias         : Ethernet0
+\\SourceAddress          : [PROXY IP]
+\\PingSucceeded          : True
+\\PingReplyDetails (RTT) : 0 ms
+\\TcpTestSucceeded       : True
+
